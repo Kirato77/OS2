931ac2437b8b4d949e66040eb3b7239fb40d83d0
4lex42 <a.laroy@students.ephec.be>
2024-01-13 18:43:42 +0100

    Improve getRandom

--- libs/utils.h
+++ libs/utils.h
@@ -5,7 +5,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-int getRandom(int min, int max) {
+int getRandom(int longueur) {
+
+    int vmax = 64;
+    int vmin = 44;
+
+    int tmax = (longueur*1000)/vmin;
+    int tmin = (longueur*1000)/vmax;
 
     // Initialisation of the seed
     srand(time(NULL));
@@ -14,7 +20,7 @@ int getRandom(int min, int max) {
     int randomNumber = rand();
 
     // Division between max and min
-    int number = randomNumber % (max - min + 1) + min;
+    int number = randomNumber % (tmax - tmin + 1) + tmin;
 
     return number;
 }
