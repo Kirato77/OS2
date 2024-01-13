dee75bedbec96be6475e17feb939357faa27e7be
4lex42 <a.laroy@students.ephec.be>
2024-01-13 20:49:42 +0100

    add getPilots

--- libs/utils.h
+++ libs/utils.h
@@ -25,7 +25,61 @@ float getRandom(int circuitLength) {
     return result;
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iconv.h>
 
+#define MAX_LINE_SIZE 1024
+#define MAX_FIELD_SIZE 256
+
+void getPilots() {
+    FILE *file = fopen("data/Liste des pilotes.csv", "r");
+
+    if (file == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE_SIZE];
+    char *token;
+
+    iconv_t conversion = iconv_open("UTF-8", "ISO-8859-1");
+
+    if (conversion == (iconv_t)-1) {
+        perror("Erreur lors de l'initialisation de la conversion de caractères");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        line[strcspn(line, "\n")] = '\0';
+
+        token = strtok(line, ",");
+
+        while (token != NULL) {
+            size_t in_size = strlen(token);
+            size_t out_size = MAX_FIELD_SIZE;
+            char output[MAX_FIELD_SIZE];
+
+            char *output_ptr = output;  // Utiliser un pointeur de type char *
+
+            if (iconv(conversion, &token, &in_size, &output_ptr, &out_size) == (size_t)-1) {
+                perror("Erreur lors de la conversion de caractères");
+                exit(EXIT_FAILURE);
+            }
+
+            *output_ptr = '\0';  // Assurez-vous que la chaîne de sortie est terminée correctement
+            printf("%s\t", output);
+
+            token = strtok(NULL, ",");
+        }
+
+        printf("\n");
+    }
+
+    iconv_close(conversion);
+    fclose(file);
+}
 
 
 #endif
