3500316b48b0cfbcfe086682f8baf2b636c89806
Kirato <justkirato@gmail.com>
2024-01-15 17:13:50 +0100

    fix got tracking

--- cmake-build-debug/libs/cJSON/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kirato/OS_F1/OS2/libs/cJSON/fuzzing
-# Build directory: /home/kirato/OS_F1/OS2/cmake-build-debug/libs/cJSON/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
